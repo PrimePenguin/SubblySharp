cb9430712fe5bb65f1c496ccaf0bb09d777a9dc8
jisha <jisha@primepenguin.com>
2023-02-02 18:53:58 +0530

    response changes

--- SubblySharp/SubblyResponse.cs
+++ SubblySharp/SubblyResponse.cs
@@ -4,8 +4,12 @@ namespace SubblySharp
 {
     public  class SubblyResponse
     {
+
         [JsonProperty("message")]
         public string Message { get; set; }
+
+        [JsonProperty("errors")]
+        public Errors Errors { get; set; }
     }
 
 }
